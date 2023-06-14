22af0020d6f9bfcf98afc210eb95f9bcbe8b9493
nikolai1312 <nicolasedesouza@gmail.com>
2023-06-14 10:40:25 -0300

    fix: adjusting routes

--- src/UnProtectedRoutes.tsx
+++ src/UnProtectedRoutes.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { validateToken } from "./helpers/validateToken";
 
-export function UnProtectedRoute() {
+export function UnProtectedRoutes() {
     const isAuthenticated = validateToken();
 
     return isAuthenticated ? <Navigate to="/home" /> : <Outlet />
